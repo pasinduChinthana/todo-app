700b18e33849e82c2412373fb6774ca883064364
ravinduf <ravinduf@rocketmail.com>
2020-10-15 11:26:07 +0530

    created header and updated layout.js and rendered layout in App

--- src/Layout.js
+++ src/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from './components/Header'
 
-const Layout = () => {
+const Layout = (props) => {
     return (
         <div>
-            
+            <Header />
+            {props.children}
         </div>
     )
 }
