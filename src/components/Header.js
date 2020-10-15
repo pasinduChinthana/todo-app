5ab38d66c6b07201e6ff895428e2d06cc56f21ce
ravinduf <ravinduf@rocketmail.com>
2020-10-15 11:47:36 +0530

    styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 const Header = () => {
     return (
-        <Container className="mt-3 p-4 bg-light">
+        <Container className="mt-3 p-4 bg-dark text-white text-center">
             <h1 className="display-4">Todo-List App</h1>
         </Container>
     )
