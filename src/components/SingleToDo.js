b5c49f9665590581217ab333f3e43bd30478e7ae
edpchinthana <edpchinthana@outlook.com>
2020-10-18 12:35:38 +0530

    conflicts resolved

--- src/components/SingleToDo.js
+++ src/components/SingleToDo.js
@@ -5,7 +5,6 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 export default function getSingleToDo(props){
 
